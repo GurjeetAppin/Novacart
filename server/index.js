f09b4c178163236d8574d9b3eec6cfecd6a5c33c
Gurjeet Singh <140533763+GurjeetAppin@users.noreply.github.com>
2026-07-15 18:00:53 +0530

    Update the code
    # Conflicts:
    #	package.json
    #	server/index.js

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv"); // get the configartion form this package.
 const connectDB = require('./config/db'); // import the database connection code.
+const path = require("path");
+
 dotenv.config(); // This is get all the configartion from the dotenv file and set inside a index.js file
 connectDB(); // Run the mongoDb connection 
 const app = express(); // Create an instance of the Express application. The app variable will be used to define routes, middleware, and other configurations for the server.
 // Create an instance of the Express application
 app.use(cors(
     {
-        origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+        origin: ["http://localhost:3000", "http://127.0.0.1:3000", process.env.FRONTEND_URL],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
@@ -18,10 +20,11 @@ app.use(express.json()); // This is used to parse the json data from the request
 app.use(express.urlencoded({ extended: true })); // This is used to parse the urlencoded data from the request body.
 
 app.get("/", (req, res) => {
- res.send("Novacart backend is working properly!");
+    res.send("Novacart backend is working properly!");
 });
+
 // User/Auth routes
-app.use("/api/auth", require('./routes/authRoutes')); 
+app.use("/api/auth", require('./routes/authRoutes'));
 
 // Products Routes
 app.use("/api/products", require("./routes/productRoutes"));
@@ -31,6 +34,18 @@ app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/payment", require("./routes/paymentRoutes"));
 // Analytics Routes (This is use for Admin)
 app.use("/api/analytics", require("./routes/analyticsRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.send("Novacart backend is working properly!");
+    });
+}
 
 // Cart Routes
 //app.use("/api/cart", require("./routes/cartRoutes"));
